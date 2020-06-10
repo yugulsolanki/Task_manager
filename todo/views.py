a87c248a9b5874745a234e75aed959c9a94cfdd2
Yugul Solanki <yugul1solanki@gmail.com>
2020-06-10 22:51:37 +0530

    first commit

---
+++ todo/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
+from .forms import TodoForm
+
+
+def home(request):
+    return render(request, 'todo/home.html')
+
+def signupuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {'form':UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html', {'form':UserCreationForm(),'error':'That username is already taken. Please choose another one.'})
+        else:
+             return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'Password did not match.'})
+
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/loginuser.html', {'form':AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html', {'form':AuthenticationForm(), 'error':'Username and password did not match.'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
+
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.html')
+
+
+def createtodo(request):
+    if request.method == 'GET':
+        return render(request, 'todo/createtodo.html', {'form':TodoForm()})
+    else:
+        try:
+            form = TodoForm(request.POST)
+            newtodo = form.save(commit=False)
+            newtodo.user = request.user
+            newtodo.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/createtodo.html', {'form':TodoForm(), 'error':'Bad data passed in. Try again'})
